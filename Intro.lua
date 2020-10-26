2685cc8192e81e94c422cd37af2bc1ec0772d1f6
JalapenoGuy <69606019+JalapenoGuy@users.noreply.github.com>
2020-10-25 17:03:27 -0700

    Update Intro.lua

--- Intro.lua
+++ Intro.lua
@@ -1,1 +1,13 @@
-print("Candi Has Injected")
+game.StarterGui:SetCore("SendNotification", {Title="Candi Has Injected"; Text="Developed By Shade & JalapenoGuy"; Duration=15;})
+local StarterGui = game:GetService("StarterGui")
+repeat
+	wait() 
+	local Success = pcall(function()
+		StarterGui:SetCore("ChatMakeSystemMessage", {
+			Text = "Candi Has Injected!"; 
+			Color = Color3.fromRGB(255, 0, 102);
+			Font = Enum.Font.SourceSansBold; 
+			TextSize = 18 
+		})
+	end)
+until Success
