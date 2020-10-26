f9d2a9899bb6782a936fc70419e7fe02b86fa3bf
JalapenoGuy <69606019+JalapenoGuy@users.noreply.github.com>
2020-10-26 09:14:58 -0700

    Update Intro.lua

--- Intro.lua
+++ Intro.lua
@@ -179,7 +179,7 @@ local function POKTC_fake_script() -- Frame.LocalScript
 	object.Position = UDim2.new(0.0, 0, 1.2, 0)
 	
 	wait()
-	object:TweenPosition(UDim2.new(0, 0, 0.753, 0), nil, nil, 3)
+	object:TweenPosition(UDim2.new(0, 0, 0, 0), nil, nil, 3)
 	wait(10)
 	for i = 0.7, 1, 0.1 do
 		wait(0.1)
